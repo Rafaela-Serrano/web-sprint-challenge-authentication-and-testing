62317ac05e1ac970c18d82082d15e6c47d3aae0d
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-11-08 17:28:12 -0600

    complited all test each test with two expect per api method

--- api/server.test.js
+++ api/server.test.js
@@ -1,8 +1,8 @@
 const request = require ('supertest');
 const db = require ('../data/dbConfig');
-const jokes_router = require('../api/jokes/jokes-router');
-const auth_router = require('../api/auth/auth-router');
-
+// const jokes_router = require('./jokes/jokes-router');
+// const auth_router = require('./auth/auth-router');
+const server = require ('./server');
 
 
 beforeAll( async () => {
@@ -17,11 +17,34 @@ describe( 'server.js' , () => {
 })
 
 describe('[POST]/register', () => {
-  it('response with a new created user', async() => {
-    const response = await request(auth_router).post('/register').send({
-      username:'Paul',password:'12345'
-    })
+  it('response with a status code 200 and a newId ', async() => {
+    const response = await request(server)
+    .post('/api/auth/register')
+    .send({username:'Paul', password:'12345'})
 
     expect(response.status).toBe(200)
+    expect(response.body.id).toBe(1)
+  })
+})
+
+describe('[POST]/login', () => {
+  it('response with a status code 200 and a welcome message', async()=>{
+    const response = await request(server)
+    .post('/api/auth/login')
+    .send({username:'Paul', password:'12345'})
+
+    expect(response.status).toBe(200)
+    expect(response.body.message).toEqual('welcome,Paul')
+  })
+})
+
+describe('[GET]/',()=>{
+  it('response with a error message and a status code 401', async()=>{
+    const response = await request(server)
+    .get('/api/jokes/')
+    .send()
+
+    expect(response.status).toBe(401)
+    expect(response.body.message).toEqual('token required')
   })
 })
