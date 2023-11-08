9b97dc284695a8962673536fdf8a5760e42217c4
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-11-08 16:39:23 -0600

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,11 +1,27 @@
 const request = require ('supertest');
 const db = require ('../data/dbConfig');
-const jokes_router = require('../api/jokes/jokes-router')
-const auth_router = require('../api/auth/auth-router')
+const jokes_router = require('../api/jokes/jokes-router');
+const auth_router = require('../api/auth/auth-router');
 
-// Write your tests here
+
+
+beforeAll( async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+
 describe( 'server.js' , () => {
   it('should set testing enviroment', () => {
     expect(process.env.NODE_ENV).toBe('testing')
+  })
+})
+
+describe('[POST]/register', () => {
+  it('response with a new created user', async() => {
+    const response = (await request(auth_router).post('/register')).send({
+      username:'Paul',password:'12345'
+    })
+
+    expect(response.status).toBe(200)
   })
 })
