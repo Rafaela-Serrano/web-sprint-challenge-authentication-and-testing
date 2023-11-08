efaa912990ed7244bd59dbbfc399b07182567881
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-11-08 10:22:18 -0600

    Created middleware helpers for api functionality

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,38 @@
+const auth_model = require ('../auth/auth-model')
+
+function checkReqBodyUsernamePassword ( req, res, next ) {
+    try{
+        if(!req.body.username || !req.body.password){
+            next({
+                status:422,
+                message:"username and password required"
+            })
+        }else{
+            next()
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
+async function checkUsernameNotTaken ( req, res, next ) {
+    try{
+        const username = await auth_model.findBy({username:req.body.username})
+
+        if(!username.length){
+            next()
+        }else{
+            next({
+                status:422,
+                message:"username taken"
+            })
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {
+    checkReqBodyUsernamePassword,
+    checkUsernameNotTaken
+}
