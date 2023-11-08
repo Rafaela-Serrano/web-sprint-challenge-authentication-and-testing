af735f77b16f2976609f1220114153b191826403
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-11-08 13:28:27 -0600

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -2,27 +2,22 @@ const {JWT_SECRET} = require ('../secrets/index');
 const jwt = require('jsonwebtoken');
 
 function restricted (req, res, next) {
-
-  const token = req.headers.authorization;
-
-  if(!token){
-    next({
-      status:401,
-      message:'token required'
-    })
-  }else if(token){
-    jwt.verify(token,JWT_SECRET,(err,decoded) => {
+  const token = req.headers.authorization
+  if(token){
+    jwt.verify(token,JWT_SECRET,(err,decoded)=>{
       if(err){
-        next({
-          status:401,
-          message:'token invalid'
-        })
+        next({status:401,message:"token invalid"})
       }else{
-        req.decodedJwt = decoded
         next()
       }
     })
-  }}
+  }else{
+    next({
+      status:401,
+      message:"token required"
+    })
+  }
+}
 
   module.exports = restricted
   
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,11 @@
+const request = require ('supertest');
+const db = require ('../data/dbConfig');
+const jokes_router = require('../api/jokes/jokes-router')
+const auth_router = require('../api/auth/auth-router')
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+describe( 'server.js' , () => {
+  it('should set testing enviroment', () => {
+    expect(process.env.NODE_ENV).toBe('testing')
+  })
 })
